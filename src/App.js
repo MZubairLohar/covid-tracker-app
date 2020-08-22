faaa235d412b800d335d3d246fbaaf52b677d11d
MZubairLohar <zubairgituser@gmail.com>
2020-08-22 15:41:36 +0500

    first push
    creating react covid tracker app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <center>
+        tracker
+      </center>
+    </div>
+  );
+}
+
+export default App;
